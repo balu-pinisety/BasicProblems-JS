38d1a713791daa509d2ddc971059379f7bed7449
balu-pinisety <manikantapandu225@gmail.com>
2021-08-03 06:33:21 +0530

    Added: Functions Practice Problem

---
+++ Functions/UnitConvertTemp.js
@@ -0,0 +1,26 @@
+console.log("Program for Unit Conversion of Temperature");
+console.log("1.Celsius to Farhenheit\n2.Farhenheit to Celsius");
+let number = Math.floor(Math.random() *10) % 2 + 1;
+console.log("Option is : "+number);
+switch (number) {
+    case 1:
+        console.log("Converting Celsius to Farhenheit");
+        let random1 = Math.floor(Math.random() *100);
+        console.log("Value of Celsius: "+random1);
+        degC_to_degF(random1);
+        break;
+    default:
+        console.log("Converting Farhenheit to Celsius");
+        let random2 = Math.floor(Math.random() *1000) % (212-32) + 32;
+        console.log("Value of Farhenheit: "+random2);
+        degF_to_degC(random2);
+}
+
+function degC_to_degF(degC) {
+    degF = (degC * 9 / 5) + 32;
+    console.log(degC+" Celsius in Farhenheit: " + degF);
+}
+function degF_to_degC(degF) {
+    degC = (degF - 32) * 5 / 9;
+    console.log(degF+" Farhenheit in Celsius: " + degC);
+}
